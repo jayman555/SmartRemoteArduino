e10fd244592141a651a88499b58f9a72477c23e2
jayman555 <jay_hansen1997@yahoo.com.au>
2015-04-10 15:46:00 +1000

    firstcommit

---
+++ BTmodule.ino
@@ -0,0 +1,97 @@
+#include <I2C.h>
+#include <LaserSupport.h>
+#include <NewPing.h>
+#include <MotorMovement.h>
+
+//DEFINES/CONSTANTS
+#define SonarTriggerPin  14
+#define SonarEchoPin     15
+#define SonarMaxDistance 255
+
+//OBJECTS
+AsymmetricMotorPair engines(10, 11, 12, 13, 9, 8);
+NewPing sonarObject(SonarTriggerPin, SonarEchoPin, SonarMaxDistance);
+I2CLaser laserObject;
+
+//GLOBAL VARIABLES
+command currentCommand = STOP;
+
+void setup()
+{
+	//set data rate for the Serial port
+	Serial.begin(9600);
+}
+
+void loop()
+{
+    //query the serial interface to check if there are bytes to read
+	if (Serial.available())
+	{
+		currentCommand = convertToCommand(Serial.read());
+	}
+
+	switch (currentCommand)
+	{
+	case DRIVEFORWARDS:
+		if (sonarObject.convert_cm(
+			sonarObject.ping_median()) > 20)         { engines.driveForward(20, 20); }
+		else								         { currentCommand = STOP;        }
+		break;
+	case DRIVEBACKWARDS:
+		if (laserObject.getLaserDistance() > 20)     { engines.driveBackward(20, 20); }
+		else						                 { currentCommand = STOP;         }
+		break;
+	case TURNLEFT:
+		engines.turnLeft(20);
+		break;
+	case TURNRIGHT:
+		engines.turnRight(20);
+		break;
+	case ROTATELEFT:
+		engines.turnLeftOnPoint(20, 20);
+		break;
+	case ROTATERIGHT:
+		engines.turnRightOnPoint(20, 20);
+		break;
+	case STOP:
+		engines.stop();
+		break;
+	default: 
+		//as backup 
+		engines.stop();
+		break;
+	}
+}
+
+//The purpose was to make the code more user friendy to my engineering friends who cant into programming
+//Because the character codes might have not been as informative but the enumeration is.
+command convertToCommand(char incomingChar)
+{
+	switch (incomingChar)
+	{
+	case '0':
+		return STOP;
+		break;
+	case '2':
+		return DRIVEBACKWARDS;
+		break;
+	case '4':
+		return TURNLEFT;
+		break;
+	case '6': 
+		return TURNRIGHT;
+		break;
+	case '8':
+		return DRIVEFORWARDS;
+		break;
+	case 'E':
+		return ROTATELEFT;
+		break;
+	case 'F':
+		return ROTATERIGHT;
+		break;
+	default:
+		return STOP;
+		break;
+	}
+}
